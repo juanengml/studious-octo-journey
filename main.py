b61dd188f5986cc9205abf78a1500f453063eab5
juan <juanengml@gmail.com>
2025-09-17 16:06:48 -0300

    ok vamos laaaa

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
+import os
 
 app = FastAPI()
 
 # Configuração dos templates
 templates = Jinja2Templates(directory="templates")
 
-# Montar arquivos estáticos
-app.mount("/static", StaticFiles(directory="static"), name="static")
+# Montar arquivos estáticos apenas se o diretório existir
+if os.path.exists("static"):
+    app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Modelo do item
 class Item(BaseModel):
